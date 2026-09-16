d839e77e2f32ab0f78d168998ac99a222adf9003
apple <apple@Ayush-MacBook.local>
2026-09-16 13:06:45 +0530

    feat: Update default Main Sheet Web App URL to new high-speed deployment

--- app.js
+++ app.js
@@ -29,8 +29,8 @@
   ];
 
   const DEFAULT_SETTINGS = {
-    scriptUrl: 'https://script.google.com/macros/s/AKfycbyOJJkVMJw5IEDyG14mrwrjeex3hsuiUnuB-vVT_wMQqhHMsZlgyKIr-dItwiYzHMz4/exec',
-    mainSheetScriptUrl: 'https://script.google.com/macros/s/AKfycbyOJJkVMJw5IEDyG14mrwrjeex3hsuiUnuB-vVT_wMQqhHMsZlgyKIr-dItwiYzHMz4/exec',
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbwmZYOR8h2Z-laoymuUdOLsYs3ps_lyoUfbkS-klR-zumKmU7QmOARV7eebmQ6yntE/exec',
+    mainSheetScriptUrl: 'https://script.google.com/macros/s/AKfycbwmZYOR8h2Z-laoymuUdOLsYs3ps_lyoUfbkS-klR-zumKmU7QmOARV7eebmQ6yntE/exec',
     trackingSheetScriptUrl: 'https://script.google.com/macros/s/AKfycbxZ9jDxeFTNXH5hdvN_PsuWH76iOJkZ4JZFKEgIAVFzjonrpJyRt783HZLucXdhlZcr/exec',
     sheetCsvUrl: 'https://docs.google.com/spreadsheets/d/11J3WSXNFfu5aARNMBX3HQazajsfzBjj7wX9MWyVVBRk/export?format=csv&gid=1608276684',
     theme: 'light',
